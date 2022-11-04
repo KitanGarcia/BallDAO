1ee6e7a292c6b0df85dabffadd3434f78971a63d
Kitan Garcia <kitangar@gmail.com>
2022-11-04 15:22:58 -0700

    deploy nft drop

--- scripts/2-deploy-drop.js
+++ scripts/2-deploy-drop.js
@@ -1,35 +1,31 @@
 import { AddressZero } from "@ethersproject/constants";
-import sdk from "./1-initialize-sdk.js";
 import { readFileSync } from "fs";
+import sdk from "./1-initialize-sdk.js";
 
 (async () => {
   try {
     const editionDropAddress = await sdk.deployer.deployEditionDrop({
-      // The collection's name, ex. CryptoPunks
-      name: "NarutoDAO Membership",
-      // A description for the collection.
-      description: "A DAO for fans of Naruto.",
-      // The image that will be held on our NFT! The fun part :).
-      image: readFileSync("scripts/assets/naruto.png"),
-      // We need to pass in the address of the person who will be receiving the proceeds from sales of nfts in the contract.
-      // We're planning on not charging people for the drop, so we'll pass in the 0x0 address
-      // you can set this to your own wallet address if you want to charge for the drop.
+      // Name of NFT collection
+      name: "BallDAO Membership",
+      description:
+        "A DAO for those infatuated with the art of megging and freestyle",
+      image: readFileSync("scripts/assets/ball.png"),
+      // The address of the person who will be receiving proceeds from sales in the contract
+      // We use 0x0 here since we're not charging people for the drop
       primary_sale_recipient: AddressZero,
     });
 
-    // this initialization returns the address of our contract
-    // we use this to initialize the contract on the thirdweb sdk
-    const editionDrop = await sdk.getContract(editionDropAddress, "edition-drop");
-
-    // with this, we can get the metadata of our contract
+    // Returns the address of our contract
+    // Used to initialize the contract on thirdweb sdk
+    const editionDrop = await sdk.getContract(editionDropAddress);
     const metadata = await editionDrop.metadata.get();
 
     console.log(
-      "✅ Successfully deployed editionDrop contract, address:",
-      editionDropAddress,
+      "Successfully deployed edition contract, address: ",
+      editionDropAddress
     );
-    console.log("✅ editionDrop metadata:", metadata);
+    console.log("editionDrop metadata: ", metadata);
   } catch (error) {
-    console.log("failed to deploy editionDrop contract", error);
+    console.log("Failed to deploy editionDrop contract: ", error);
   }
 })();
