5f1cc8d1d6a856f125793dcff8350157c0b70633
Kitan Garcia <kitangar@gmail.com>
2022-11-04 15:14:18 -0700

    initialize SDK

--- scripts/1-initialize-sdk.js
+++ scripts/1-initialize-sdk.js
@@ -1,38 +1,41 @@
 import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+import ethers from "ethers";
 
-// Importing and configuring our .env file that we use to securely store our environment variables
+// Importing and configuring our .env file
 import dotenv from "dotenv";
 dotenv.config();
 
-// Some quick checks to make sure our .env is working.
+// Checks to make sure .env is working
 if (!process.env.PRIVATE_KEY || process.env.PRIVATE_KEY === "") {
-  console.log("🛑 Private key not found.");
+  console.log("Private key not found");
 }
 
 if (!process.env.QUICKNODE_API_URL || process.env.QUICKNODE_API_URL === "") {
-  console.log("🛑 QuickNode API URL not found.");
+  console.log("Quicknode API not found");
 }
 
 if (!process.env.WALLET_ADDRESS || process.env.WALLET_ADDRESS === "") {
-  console.log("🛑 Wallet Address not found.");
+  console.log("Wallet address not found");
 }
 
-const sdk = ThirdwebSDK.fromPrivateKey(
-  // Your wallet private key. ALWAYS KEEP THIS PRIVATE, DO NOT SHARE IT WITH ANYONE, add it to your .env file and do not commit that file to github!
-  process.env.PRIVATE_KEY,
-  // RPC URL, we'll use our QuickNode API URL from our .env file.
-  "goerli"
+// Get RPC URL using Quicknode
+const provider = new ethers.providers.JsonRpcProvider(
+  process.env.QUICKNODE_API_URL
 );
 
+const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+const sdk = new ThirdwebSDK(wallet);
+
+// Make sure SDK initialized correctly
 (async () => {
   try {
     const address = await sdk.getSigner().getAddress();
-    console.log("👋 SDK initialized by address:", address)
-  } catch (err) {
-    console.error("Failed to get apps from the sdk", err);
+    console.log("SDK initialized by address: ", address);
+  } catch (error) {
+    console.error("Failed to get apps from the sdk", error);
     process.exit(1);
   }
 })();
 
-// We are exporting the initialized thirdweb SDK so that we can use it in our other scripts
+// Exporting the initialized thirdweb SDK to use in other scripts
 export default sdk;
