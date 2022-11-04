545f548f11ab18b883e3d2fd72eb3285bb9b093a
Kitan Garcia <kitangar@gmail.com>
2022-11-04 15:48:57 -0700

    set claim condition

--- scripts/4-set-claim-condition.js
+++ scripts/4-set-claim-condition.js
@@ -3,26 +3,34 @@ import { MaxUint256 } from "@ethersproject/constants";
 
 (async () => {
   try {
-    const editionDrop = await sdk.getContract("INSERT_EDITION_DROP_ADDRESS", "edition-drop");
+    const editionDrop = await sdk.getContract(
+      "0x00C879218ad295CCfb8a8057547795bafCF4D621",
+      "edition-drop"
+    );
     // We define our claim conditions, this is an array of objects because
     // we can have multiple phases starting at different times if we want to
-    const claimConditions = [{
-      // When people are gonna be able to start claiming the NFTs (now)
-      startTime: new Date(),
-      // The maximum number of NFTs that can be claimed.
-      maxClaimable: 50_000,
-      // The price of our NFT (free)
-      price: 0,
-      // The amount of NFTs people can claim in one transaction.
-      maxClaimablePerWallet: 1,
-      // We set the wait between transactions to unlimited, which means
-      // people are only allowed to claim once.
-      waitInSeconds: MaxUint256,
-    }]
+    const claimConditions = [
+      {
+        // When people are gonna be able to start claiming the NFTs (now)
+        startTime: new Date(),
+        // The maximum number of NFTs that can be claimed.
+        maxClaimable: 50_000,
+        // The price of our NFT (free)
+        price: 0,
+        // The amount of NFTs people can claim in one transaction.
+        maxClaimablePerWallet: 1,
+        // We set the wait between transactions to unlimited, which means
+        // people are only allowed to claim once.
+        waitInSeconds: MaxUint256,
+      },
+    ];
 
+    // Interacts with our deployed contract on-chain
+    // 0 is the tokenId. Here all the NFTs are the same, so will have the same tokenId
     await editionDrop.claimConditions.set("0", claimConditions);
-    console.log("✅ Sucessfully set claim condition!");
+    console.log("Sucessfully set claim condition!");
   } catch (error) {
     console.error("Failed to set claim condition", error);
   }
 })();
+
