71fad1fb1cec3cb86993eea7bef1070b55f88bdb
Kitan Garcia <kitangar@gmail.com>
2022-11-07 12:35:27 -0800

    create token supply

--- scripts/6-print-money.js
+++ scripts/6-print-money.js
@@ -3,16 +3,24 @@ import sdk from "./1-initialize-sdk.js";
 (async () => {
   try {
     // This is the address of our ERC-20 contract printed out in the step before.
-    const token = await sdk.getContract("INSERT_TOKEN_ADDRESS", "token");
-    // What's the max supply you want to set? 1,000,000 is a nice number!
+    const token = await sdk.getContract(
+      "0x36aE2b29340d7006Bf5F5509e7B46fbeFAec9535",
+      "token"
+    );
+    // Set max supply to 1 million
     const amount = 1_000_000;
-    // Interact with your deployed ERC-20 contract and mint the tokens!
+    // Interact with deployed ERC-20 contract and mint the tokens!
     await token.mint(amount);
     const totalSupply = await token.totalSupply();
 
-    // Print out how many of our token's are out there now!
-    console.log("✅ There now is", totalSupply.displayValue, "$HOKAGE in circulation");
+    // Print out how many of our token are out there now!
+    console.log(
+      "There now are",
+      totalSupply.displayValue,
+      "$BALL in circulation"
+    );
   } catch (error) {
     console.error("Failed to print money", error);
   }
 })();
+
